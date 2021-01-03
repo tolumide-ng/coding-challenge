9ff5a8add47acb49358af5cd8171646f460b06a6
tolumide-ng <tolumideshopein@gmail.com>
2021-01-03 10:45:23 +0100

    dynamic programming: tabulated how sum

--- src/dynamic_programming/how_sum.rs
+++ src/dynamic_programming/how_sum.rs
@@ -4,11 +4,14 @@
 
 pub mod how_sum {
 
+    use std::cell::RefCell;
     use std::collections::HashMap;
+
+    type ReturnType = Option<Vec<usize>>;
 
     struct HowSum {
         coins: Vec<usize>,
-        memo: HashMap<i64, Option<Vec<usize>>>,
+        memo: HashMap<i64, ReturnType>,
     }
 
     impl HowSum {
@@ -20,9 +23,9 @@ pub mod how_sum {
         }
     }
 
-    pub fn recursive_how_sum(target_sum: i64, coins: Vec<usize>) -> Option<Vec<usize>> {
+    pub fn recursive_how_sum(target_sum: i64, coins: Vec<usize>) -> ReturnType {
         impl HowSum {
-            pub fn get_recursive_sum(&self, target_sum: i64) -> Option<Vec<usize>> {
+            pub fn get_recursive_sum(&self, target_sum: i64) -> ReturnType {
                 if target_sum == 0 {
                     return Some(Vec::new());
                 }
@@ -48,9 +51,9 @@ pub mod how_sum {
         return result.get_recursive_sum(target_sum);
     }
 
-    pub fn memoized_how_sum(target_sum: i64, coins: Vec<usize>) -> Option<Vec<usize>> {
+    pub fn memoized_how_sum(target_sum: i64, coins: Vec<usize>) -> ReturnType {
         impl HowSum {
-            fn get_memoized_sum(&mut self, target_sum: i64) -> Option<Vec<usize>> {
+            fn get_memoized_sum(&mut self, target_sum: i64) -> ReturnType {
                 if self.memo.contains_key(&target_sum) {
                     let value = self.memo.get(&target_sum);
                     return value.unwrap().clone();
@@ -90,6 +93,42 @@ pub mod how_sum {
         let mut result = HowSum::new(coins);
         return result.get_memoized_sum(target_sum);
     }
+
+    pub fn tabulated_how_sum(target_sum: usize, coins: Vec<usize>) -> ReturnType {
+        impl HowSum {
+            pub fn get_tabulated_sum(&self, target_sum: usize) -> ReturnType {
+                let mut vec_store: Vec<Option<Vec<usize>>> = vec![None; target_sum as usize + 1];
+
+                vec_store[0] = Some(vec![]);
+
+                for value in 0..=target_sum as usize {
+                    for coin in &self.coins {
+                        let check_is_some = vec_store[value].is_some();
+                        if check_is_some {
+                            let new_target = value + *coin;
+                            if new_target <= target_sum {
+                                if vec_store[new_target].is_none() {
+                                    let mut new_val = vec_store[value].clone().unwrap();
+
+                                    new_val.push(*coin);
+                                    vec_store[new_target] = Some(new_val);
+                                } else if vec_store[new_target].is_some() {
+                                    let mut new_val = vec_store[value].clone().unwrap();
+                                    new_val.push(*coin);
+                                    vec_store[new_target] = Some(new_val);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return vec_store[target_sum as usize].clone();
+            }
+        }
+
+        let result = HowSum::new(coins);
+        return result.get_tabulated_sum(target_sum);
+    }
 }
 
 #[test]
@@ -115,4 +154,13 @@ fn test_memoized_how_sum() {
     );
     assert_eq!(memoized_how_sum(7, vec![2, 4]).is_none(), true);
     assert_eq!(memoized_how_sum(300, vec![7, 14]).is_none(), true);
+}
+
+#[test]
+fn test_tabulated_how_sum() {
+    use how_sum::tabulated_how_sum;
+
+    assert!(tabulated_how_sum(7, vec![2, 3]).unwrap().len() > 2);
+    assert_eq!(tabulated_how_sum(7, vec![5, 3, 4, 7]).is_some(), true);
+    assert_eq!(tabulated_how_sum(7, vec![2, 4]).is_none(), true);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod dynamic_programming;
 // use dynamic_programming::can_sum::can_sum;
 // use dynamic_programming::how_sum::how_sum;
 // use dynamic_programming::best_sum::best_sum;
-use dynamic_programming::all_construct::all_construct::recursive_all_construct;
+use dynamic_programming::how_sum::how_sum::tabulated_how_sum;
 
 fn main() {
     println!(
@@ -11,6 +11,6 @@ fn main() {
         // best_sum::memoized_best_sum(100, vec![1, 2, 5, 25, 30])
         // best_sum::memoized_best_sum(100, vec![1, 2, 5, 25, 30])
         // recursive_all_construct("abcdef", vec!["ab", "abc", "cd", "def", "abcd", "ef", "c"]),
-        recursive_all_construct("abcdef", vec!["ab", "abc", "cd", "def", "abcd", "ef", "c"]),
+        tabulated_how_sum(7, vec![5, 3, 4, 7])
     );
 }
