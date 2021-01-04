83eab32ad256452f620473da6e347a496e176206
tolumide-ng <tolumideshopein@gmail.com>
2021-01-04 03:26:43 +0100

    dynamic: tabulated can construct

--- src/dynamic_programming/can_construct.rs
+++ src/dynamic_programming/can_construct.rs
@@ -83,6 +83,38 @@ pub mod can_construct {
         let mut result = CanConstruct::new(dic);
         return result.get_memoized_construct(target);
     }
+
+    pub fn tabulated_can_construct(target: &str, dic: Vec<&'static str>) -> bool {
+        // where dic is dictionary of words
+        impl CanConstruct {
+            pub fn get_tabulated_can_construct(&self, target: &str) -> bool {
+                // let target = target.trim();
+                let mut vec_store = vec![false; target.len() + 1];
+
+                let the_dic = self.dic.clone();
+
+                // it takes nothing to make 0 words
+                vec_store[0] = true;
+
+                for value in 0..=target.len() {
+                    for word in &the_dic {
+                        if vec_store[value] {
+                            let word_len = word.len();
+                            if target.get(value..word_len).is_some()
+                                && value + word_len <= target.len()
+                            {
+                                vec_store[value + word_len] = true;
+                            }
+                        }
+                    }
+                }
+
+                return vec_store[target.len()];
+            }
+        }
+        let result = CanConstruct::new(dic);
+        return result.get_tabulated_can_construct(target);
+    }
 }
 
 #[test]
@@ -127,6 +159,34 @@ fn test_memoized_can_construct() {
     );
     assert_eq!(
         memoized_can_construct(
+            "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+            vec!["e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"]
+        ),
+        false
+    );
+}
+
+#[test]
+fn test_tabulted_construct() {
+    use can_construct::tabulated_can_construct;
+
+    assert_eq!(
+        tabulated_can_construct("tolumide", vec!["t", "ol", "umide"]),
+        true
+    );
+    assert_eq!(
+        tabulated_can_construct("abcdef", vec!["ab", "abc", "cd", "def", "abcd"]),
+        true
+    );
+    assert_eq!(
+        tabulated_can_construct(
+            "skateboard",
+            vec!["bo", "rd", "ate", "t", "ska", "sk", "boar"]
+        ),
+        false
+    );
+    assert_eq!(
+        tabulated_can_construct(
             "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
             vec!["e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"]
         ),
