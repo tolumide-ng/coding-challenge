4383d60060ff0e6295593d06f6e266b94ababadc
tolumide-ng <tolumideshopein@gmail.com>
2021-01-15 12:59:07 +0100

    complete singly linked list

--- data_structures/src/linked_lists/single_ll.rs
+++ data_structures/src/linked_lists/single_ll.rs
@@ -76,21 +76,12 @@ impl LL {
         // }
 
         let mut curr_value = self.head.as_ref().unwrap();
-        // println!(
-        //     "THE WHOELE TREE BUT YOUR QUERY FIRST {:?},--------- {:#?}",
-        //     index, self.head
-        // );
 
         for _ in 0..index {
             if curr_value.next.is_some() {
                 curr_value = curr_value.next.as_ref().unwrap();
             }
         }
-
-        // println!(
-        //     "WHAT THE CURRENT VALUE STANDS FOR *********** {:#?}",
-        //     curr_value
-        // );
 
         return curr_value.value;
     }
@@ -117,6 +108,7 @@ impl LL {
 
                 loop {
                     curr_index += 1;
+
                     if curr_index == index {
                         let right_heand = curr.next.clone();
                         curr.next = Some(Box::new(Node {
@@ -143,11 +135,9 @@ impl LL {
     }
 
     fn delete_at_index(&mut self, index: i32) {
-        if index > self.total as i32 || index < 0 {
+        if index >= self.total as i32 || index < 0 {
             return;
         }
-
-        println!(":::::::::::::::::::::::YOU ESAPED THE OLD CONDITIONAL::::::::::::::::");
 
         match &mut self.head {
             Some(head) => {
@@ -162,30 +152,23 @@ impl LL {
                 }
 
                 loop {
-                    if curr_index == index {
+                    if curr_index == (index - 1) {
                         match &mut curr.next {
                             Some(the_next) => {
                                 let right_hand = the_next.clone().next;
                                 curr.next = None;
                                 curr.next = right_hand;
+
                                 self.total -= 1;
                                 break;
                             }
                             None => {
-                                println!("WITHIN THIS BEHE ---------------- {:?}", curr);
                                 curr.next = None;
-                                println!("WITHIN THIS BEHE AFTEE IMPACT :::::::::----------------::::::: {:?}", curr);
 
                                 self.total -= 1;
                                 break;
                             }
                         };
-                        // let right_hand = curr.next.clone().unwrap().next;
-
-                        // curr.next = None;
-                        // curr.next = right_hand;
-                        // self.total -= 1;
-                        // break;
                     }
                     curr_index += 1;
                     curr = match &mut curr.next {
@@ -203,67 +186,38 @@ impl LL {
 fn singly_ll() {
     let mut list = LL::new();
     list.add_at_head(2);
-    println!("EVERYTHING 1------------------->>>>>>>>> {:#?}", list);
 
-    // assert_eq!(list.get(0), 7);
+    assert_eq!(list.get(1), -1);
+
     list.delete_at_index(1);
-    println!("EVERYTHING 2------------------->>>>>>>>> {:#?}", list);
 
     list.add_at_head(2);
-    println!("EVERYTHING 3------------------->>>>>>>>> {:#?}", list);
 
     list.add_at_head(7);
-    println!("EVERYTHING 4------------------->>>>>>>>> {:#?}", list);
+    assert_eq!(list.get(1), 2);
 
     list.add_at_head(3);
-    println!("EVERYTHING 5------------------->>>>>>>>> {:#?}", list);
 
     list.add_at_head(2);
-    println!("EVERYTHING 6------------------->>>>>>>>> {:#?}", list);
 
     list.add_at_head(5);
-    println!("EVERYTHING 7------------------->>>>>>>>> {:#?}", list);
 
-    // list.add_at_tail(3);
     list.add_at_tail(5);
-    println!("EVERYTHING 8------------------->>>>>>>>> {:#?}", list);
+    assert_eq!(list.get(list.total as i32 - 1), 5);
 
-    // list.add_at_index(3, 0);
-    // list.add_at_index(1, 2);
     list.get(5);
-    println!("EVERYTHING 9------------------->>>>>>>>> {:#?}", list);
 
     list.delete_at_index(6);
-    println!("EVERYTHING 10------------------->>>>>>>>> {:#?}", list);
+    assert_eq!(list.get(0), 5);
+    assert_eq!(list.get(list.total as i32 - 1), 2);
 
     list.delete_at_index(4);
-    println!("EVERYTHING 11------------------->>>>>>>>> {:#?}", list);
 
-    // list.add_at_head(6);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
+    list.delete_at_index(0);
+    assert_eq!(list.get(0), 2);
 
-    // list.add_at_tail(4);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
+    list.add_at_index(5, 55);
 
-    // list.get(4);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
-
-    // list.add_at_head(4);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
-
-    // list.add_at_index(5, 0);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
-
-    // list.add_at_head(6);
-    // println!("EVERYTHING------------------->>>>>>>>> {:#?}", list);
-
-    assert_eq!(list.get(0), 22);
-    // list.add_at_tail(23);
-    // assert_eq!(list.get(2), 23);
-    // assert_eq!(list.get(7), -1);
-    // // println!("the list>>> {:#?}", list);
-    // assert_eq!(list.total, 4);
-    // list.delete_at_index(2);
-    // // println!(":::::::AFTER THE ACTION::::::: {:#?}", list);
-    // assert_eq!(list.total, 4);
+    assert_ne!(list.get(list.total as i32 - 1), 55);
+    assert_eq!(list.get(list.total as i32 - 1), 2);
 }
