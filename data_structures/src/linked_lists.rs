a250f2a1c656fb9d5d144e4e5bc2b8d03d38f253
tolumide-ng <tolumideshopein@gmail.com>
2021-01-21 01:20:01 +0100

    two linked list intercept

--- data_structures/src/linked_lists.rs
+++ data_structures/src/linked_lists.rs
@@ -1,4 +1,5 @@
 pub mod leet_singly_ll;
 pub mod linked_cycle;
 pub mod linked_cycle_pos;
+pub mod linked_lists_intercept;
 pub mod single_ll;
---
+++ data_structures/src/linked_lists/linked_lists_intercept.rs
@@ -0,0 +1,51 @@
+/// Find the point of intersection of two linked lists
+///
+use std::rc::Rc;
+
+struct Node {
+    value: i32,
+    next: Option<Rc<Node>>,
+}
+
+struct LinkedList {
+    first_head: Option<Rc<Node>>,
+    second_head: Option<Rc<Node>>,
+}
+
+impl LinkedList {
+    fn get_interception(&self) -> Option<Rc<Node>> {
+        let mut intercept: Option<Rc<Node>> = None;
+
+        let mut vec_safe = vec![];
+        let mut is_intercept = false;
+
+        match self.first_head {
+            Some(ref head) => {
+                let mut curr = head;
+                vec_safe.push(Rc::clone(curr));
+
+                while head.next.is_some() {
+                    curr = head.next.as_ref().unwrap();
+                    vec_safe.push(Rc::clone(curr));
+                }
+            }
+            None => return intercept,
+        }
+
+        match self.second_head {
+            Some(ref head) => {
+                let mut curr = head;
+                while head.next.is_some() && !is_intercept {
+                    vec_safe.iter().map(|val| {
+                        if assert_eq!(Rc::as_ptr(curr), Rc::as_ptr(val)) {
+                            is_intercept = true;
+                            intercept = Some(*curr);
+                        }
+                    });
+                }
+                return intercept;
+            }
+            None => return intercept,
+        }
+    }
+}
