f2b70562aa8c90e506edf29bd02753acd99c91e3
tolumide-ng <tolumideshopein@gmail.com>
2021-02-10 01:53:59 +0100

    post order traversal

--- data_structures/src/binary_tree/post_order_traversal.rs
+++ data_structures/src/binary_tree/post_order_traversal.rs
@@ -6,8 +6,9 @@ impl<T> BinaryHeap<T>
 where
     T: PartialEq + Eq + Clone + Debug + Copy + Display,
 {
-    fn recursive_in_order_traversal(&self, start: Option<usize>) -> Vec<T> {
+    fn recusrive_post_order_traversal(&self, start: Option<usize>) -> Vec<T> {
         let mut traversal: Vec<T> = vec![];
+
         if self.heap.len() > 0 {
             if start.is_none() {
                 return traversal;
@@ -16,65 +17,35 @@ where
             let curr_index = start.unwrap();
             let left_index = self.get_left_child(curr_index);
 
-            let next_left = self.recursive_in_order_traversal(left_index);
+            let next_left = self.recusrive_post_order_traversal(left_index);
 
             for value in next_left {
                 traversal.push(value);
             }
 
-            // add curr node
-            let curr_node = self.heap[curr_index].node;
-            traversal.push(curr_node);
-
+            // right node now
             let right_index = self.get_right_child(curr_index);
-            let next_right = self.recursive_in_order_traversal(right_index);
+            let next_right = self.recusrive_post_order_traversal(right_index);
 
             for value in next_right {
                 traversal.push(value);
             }
 
-            // let node_at_left = self.heap[next_left];
-            // traversal.push()
+            // add the curr value
+            let curr_node = self.heap[curr_index].node;
+            traversal.push(curr_node);
         }
 
-        return traversal;
+        traversal
     }
 }
 
 #[cfg(test)]
-mod test_inorder_traversal {
+mod test_post_order_traversal {
     use super::*;
 
     #[test]
-    fn one_element_recursive_inorder_test() {
-        let mut tree = BinaryHeap::new(10);
-
-        tree.insert(TheNode {
-            node: "A",
-            weight: 5,
-        });
-
-        let trav = tree.recursive_in_order_traversal(Some(0));
-
-        assert_eq!(trav.len(), 1);
-        assert_eq!(trav, vec!["A"]);
-    }
-
-    #[test]
-    fn double_element_recursive_inorder_test() {
-        let mut tree = BinaryHeap::new(10);
-
-        tree.insert(TheNode { node: 1, weight: 5 });
-        tree.insert(TheNode { node: 2, weight: 7 });
-
-        let trav = tree.recursive_in_order_traversal(Some(0));
-        assert_eq!(trav.len(), 2);
-        // remember we are dealing with a balanced min priority queue
-        assert_eq!(trav, vec![2, 1]);
-    }
-
-    #[test]
-    fn recursive_inorder_test() {
+    fn test_post_order_traversal() {
         let mut tree = BinaryHeap::new(10);
 
         tree.insert(TheNode {
@@ -106,8 +77,10 @@ mod test_inorder_traversal {
             weight: 4,
         });
 
-        let trav = tree.recursive_in_order_traversal(Some(0));
+        let trav = tree.recusrive_post_order_traversal(Some(0));
 
-        assert_eq!(trav, vec!["F", "B", "A", "E", "D", "J", "Q"]);
+        println!("THE REAL TREEE >>>>>>>>> {:#?}", tree);
+
+        assert_eq!(trav, vec!["F", "A", "B", "D", "Q", "J", "E"]);
     }
 }
