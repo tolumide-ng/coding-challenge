4ec2b9028a2541ada0741ec00406e4cd32309f11
tolumide-ng <tolumideshopein@gmail.com>
2021-02-10 23:19:38 +0100

    max_depth of a binary tree

---
+++ data_structures/src/binary_tree/max_depth.rs
@@ -0,0 +1,45 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if root.is_none() {
+            return 0;
+        }
+
+        let mut left: Option<Rc<RefCell<TreeNode>>> = None;
+        if root.as_ref().unwrap().borrow().left.is_some() {
+            left = Some(Rc::clone(
+                &root.as_ref().unwrap().borrow().left.as_ref().unwrap(),
+            ))
+        }
+        let mut left_result = Solution::max_depth(left);
+
+        let mut right: Option<Rc<RefCell<TreeNode>>> = None;
+        if root.as_ref().unwrap().borrow().right.is_some() {
+            right = Some(Rc::clone(
+                &root.as_ref().unwrap().borrow().right.as_ref().unwrap(),
+            ))
+        }
+        let mut right_result = Solution::max_depth(right);
+
+        return std::cmp::max(right_result, left_result) + 1;
+    }
+}
