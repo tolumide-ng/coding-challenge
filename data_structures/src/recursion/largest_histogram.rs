18ef2b129bf4ed5f88af7b46d1cc80e3b332043e
tolumide-ng <tolumideshopein@gmail.com>
2021-05-22 11:08:24 +0100

    largest histogram

--- data_structures/src/recursion/largest_histogram.rs
+++ data_structures/src/recursion/largest_histogram.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct LargestRectangle {
     stack: Vec<usize>,
     heights: Vec<i32>,
@@ -10,15 +11,23 @@ impl LargestRectangle {
         LargestRectangle {
             stack: vec![],
             heights,
-            area: -1, 
+            area: -1,
             max_area: -1,
         }
     }
 
     pub fn get_rectangle(&mut self) {
         for index in 0..self.heights.len() {
-            if self.stack.is_empty() || self.heights[index] < self.heights[self.stack.len() -1] {
-                self.update_area(index);
+            loop {
+                if let Some(stack_top) = self.stack.last() {
+                    if self.heights[index] < self.heights[*stack_top] {
+                        self.update_area(index);
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
             }
 
             self.stack.push(index);
@@ -29,10 +38,10 @@ impl LargestRectangle {
         }
     }
 
-    pub fn update_area(&mut self, index: usize){
+    pub fn update_area(&mut self, index: usize) {
         if let Some(active_index) = self.stack.pop() {
             if let Some(index_at_top) = self.stack.last() {
-                self.area = self.heights[active_index] * (index - index_at_top -1) as i32;
+                self.area = self.heights[active_index] * (index - index_at_top - 1) as i32;
             } else {
                 self.area = self.heights[active_index] * index as i32;
             }
@@ -42,25 +51,25 @@ impl LargestRectangle {
     }
 }
 
-
 pub fn get_largest_rectangle(heights: Vec<i32>) -> i32 {
     let mut histogram = LargestRectangle::new(heights);
+
     histogram.get_rectangle();
 
     return histogram.max_area;
 }
 
-
-
 #[cfg(test)]
 mod test_largest_histogram_mod {
     use crate::recursion::largest_histogram::get_largest_rectangle;
-
 
     #[test]
     fn test_largest_histogram() {
         let rectangles = vec![2, 4, 3, 6, 4];
 
         assert_eq!(get_largest_rectangle(rectangles), 12);
+
+        let rectangles = vec![2, 1, 5, 6, 2, 3];
+        assert_eq!(get_largest_rectangle(rectangles), 10);
     }
 }
